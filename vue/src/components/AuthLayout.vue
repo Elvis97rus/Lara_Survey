5376f0d47f53f9a5f6f26edb79e5c94fb099675a
Artem Shavkunov <shavkunov.a@seointellect.ru>
2022-02-21 17:46:05 +0300

    Add: Vue router, Layout Components, Migrations, custom Registration(b)

---
+++ vue/src/components/AuthLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="max-w-md w-full space-y-8">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import {LockClosedIcon} from '@heroicons/vue/solid'
+
+export default {
+  components: {
+    LockClosedIcon,
+  },
+}
+</script>
